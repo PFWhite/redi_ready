94d0db973d7eaa9ab96ef3c0f64be052fc65d64f
Patrick White <pfwhite@ufl.edu>
2017-02-23 10:23:02 -0500

    modified config example

--- config.example.py
+++ config.example.py
@@ -1,7 +1,19 @@
-token = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
-endpoint = 'http://redcap.dev/redcap/api/'
-versions = [
-    './cappy/versions/v6.16.0.json'
-]
+import os
+from cappy import get_version_files
 
-outfile_dir = '/'.join(['.', 'data'])
+token = 'XXXXXXXXXXXXXXXXXXXXXXXXX'
+endpoint = 'https://dev.redcap.org/api/'
+
+versions = get_version_files()
+
+outfile_dir = os.path.join('.', 'data')
+infile_dir = os.path.join('.', 'data_in')
+
+def get_settings_ini_path(project_dir):
+    return os.path.join(outfile_dir, project_dir, 'settings.ini')
+settings_ini_string = 'IMPORT_CONTENT_TYPE={}'
+
+def get_project_config_path(project_dir):
+    return os.path.join(outfile_dir, project_dir, 'project.config')
+project_config_string = """project_title,purpose,is_longitudinal
+"{}",{},{}"""
